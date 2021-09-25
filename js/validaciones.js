d5616844d78d11138b2926de0af308c1294fa19c
jcardonamde <yotomc17@gmail.com>
2021-09-25 09:57:08 -0500

    Modificación en textos del registro y trabajo base en js

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,5 +1,27 @@
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+    telefono: /^\d{6,10}$/, // 7 a 14 numeros.
+    direccion: /^[a-zA-Z0-9\_\-\#]{7,30}$/, // 7 a 30 caracteres
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+}
+
+const campos = {
+    nombre: false,
+    usuario: false,
+    password: false,
+    telefono: false,
+    direccion: false,
+    correo: false,
+}
 
 
+var numTelefono = document.getElementById('campoTelefono');
+var direccionUsuario = document.getElementById('campoDireccion');
 
 function validar_telefono (string) {
 
@@ -10,5 +32,33 @@ function validar_direccion (string) {
 
 }
 
+
 module.exports.validar_telefono = validar_telefono;
 module.exports.validar_direccion = validar_direccion;
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const terminos = document.getElementById('terminos');
+    if (campos.nombre && campos.usuario && campos.password && campos.telefono && campos.direccion && campos.correo && terminos.checked) {
+        formulario.reset();
+        document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+        setTimeout( () => {
+            document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+        }, 5000);
+
+        document.querySelectorAll ('.formulario__grupo-correcto').forEach((icono) => {
+            icono.classList.remove('formulario__grupo-correcto');
+        });
+    } else {
+        document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+        setTimeout( () => {
+            document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
+        }, 5000);
+    }
+});
