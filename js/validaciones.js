98cb38480681124178778e6ae565d67028f90101
jcardonamde <yotomc17@gmail.com>
2021-09-25 02:20:42 -0500

    Se modifica estructura del formulario de registros, se asignan clases y se cambias sus estilos CSS, se asocian archivo validaciones.js

---
+++ js/validaciones.js
@@ -0,0 +1,14 @@
+
+
+
+function validar_telefono (string) {
+
+}
+
+
+function validar_direccion (string) {
+
+}
+
+module.exports.validar_telefono = validar_telefono;
+module.exports.validar_direccion = validar_direccion;
